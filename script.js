0de1dff30a91fce592c54d93b49c7360e81530ad
richassingh <134148258+richassingh@users.noreply.github.com>
2023-07-17 23:14:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function fibonacci(num) {
-	if(num<=1){
-    return num;
+	if (num === 1) return 0;
+  else if (num === 2) return 1;
+  num -= 2;
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
   }
-  else{
-  return fibonacci(n-1) + fibonacci(n-2);
-  }
-}
-// your code here
+  return num2;
 }
 
 module.exports = fibonacci;
